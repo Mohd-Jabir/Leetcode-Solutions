f945e7b2ed0acfc2a5486bfba09f8f4eb8fa158a
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2025-07-19 21:51:15 +0530

    Create Split Array_by_Prime_Indices.cpp

---
+++ Arrays/Split Array_by_Prime_Indices.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool solve(int n) {
+    if(n<2) return false;
+    for(int i=2;i<=sqrt(n);++i)
+        if(n%i==0) return false;
+    return true;
+}
+    long long splitArray(vector<int>& nums) {
+    long long sum1=0,sum2=0;
+    for(int i=0;i<nums.size();i++){
+        if(solve(i)) sum1+=nums[i];
+        else sum2+=nums[i];
+    }
+    return abs(sum1-sum2);
+    }
+};
