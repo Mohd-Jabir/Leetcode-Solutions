6c8519bdc274bd4525fa46c18940723d09d9df80
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2025-07-23 22:48:08 +0530

    Create 387 || First_Unique_Character_in_a_String.cpp

---
+++ Strings/387 || First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+         unordered_map<char,int>count;
+         for(auto c:s) count[c]++;
+         for(int i=0;i<s.size();i++) {
+            if(count[s[i]]==1) return i;
+    }
+    return -1;
+    }
+};
