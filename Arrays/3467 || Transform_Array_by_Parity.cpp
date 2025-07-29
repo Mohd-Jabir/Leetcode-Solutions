6e5d51976c92be20c6814fee3ddd6f60c4ed45b9
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2025-07-29 21:16:27 +0530

    Update 3467 || Transform_Array_by_Parity.cpp

--- Arrays/3467 || Transform_Array_by_Parity.cpp
+++ Arrays/3467 || Transform_Array_by_Parity.cpp
@@ -3,9 +3,7 @@ public:
     vector<int> transformArray(vector<int>& nums) {
         int even=0,odd=0;
         vector<int>ans;
-        for(auto val:nums){
-            (val%2==0)?even++:odd++;
-        }
+        for(auto val:nums)  (val%2==0)?even++:odd++;
         for(auto val:nums){
             if(even!=0){ ans.push_back(0); even--;}
             else if(even==0&&odd!=0){ ans.push_back(1); odd--;}
